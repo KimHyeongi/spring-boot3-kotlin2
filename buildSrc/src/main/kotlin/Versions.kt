e4f5657412bff9b5729b7229882c703d41cefe01
grissom <grissom@infinistar.co.kr>
2025-04-10 13:17:00 +0900

    feat: [GRISSOM]

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,7 @@ object Versions {
     const val kotlin = "2.1.20"
     const val kotlin_ksp = "2.1.20-2.0.0"
     const val spring_boot = "3.4.4"
+    const val spring_session = "3.4.2"
     const val io_awspring_version = "3.3.0"
     const val aws_crt_version = "0.31.3"
     const val spring_cloud_aws_dep_version = "3.3.0"
@@ -13,13 +14,10 @@ object Versions {
     const val spring_boot_cloud =  "2024.0.0" //  2022.0.4
     const val kotlin_stdlib = kotlin
     const val kotlin_logging = "7.0.4"
-    const val querydsl_version = "5.1.0"
     const val logback_common = "2.0.6"
     const val logback_classic = "1.5.18"
     const val logstash_logback_encoder = "8.1"
     const val junit_jupiter = "5.11.4"
-    const val testcontainers = "1.20.4"
-    const val testcontainers_junit_jupiter = testcontainers
     const val kotest_version = "5.9.1"
     const val kotest_extensions_spring = "1.3.0"
     const val mock = "1.13.16"
@@ -29,28 +27,25 @@ object Versions {
     const val javers = "7.8.0"
     const val jackson = "2.18.2"
     const val slack = "1.45.3"
-    const val langchain4j = "1.0.0-beta1"
-    const val langchain4j_b = "1.0.0-alpha1"
     const val openfeign_querydsl_version = "6.10.1"
     const val http_client_version = "5.4.2"
-    const val kotlin_reflect = "1.9.22" // 실제 버전은 확인 필요
-    const val kotlinx_coroutines = "1.8.0" // 실제 버전은 확인 필요
-    const val spring_security = "6.2.3" // 실제 버전은 확인 필요
-    const val spring_session = "3.2.1" // 실제 버전은 확인 필요
-    // Thymeleaf
-    const val thymeleaf_extras = "3.1.3.RELEASE" // 실제 버전은 확인 필요
-    const val thymeleaf_layout = "3.4.0" // 실제 버전은 확인 필요
-    // Firebase
+    const val kotlin_reflect = "1.9.22"
+    const val kotlinx_coroutines = "1.8.0"
+    const val thymeleaf_extras = "3.1.3.RELEASE"
+    const val thymeleaf_layout = "3.4.0"
     const val firebase_admin = "9.4.3"
-    // Excel
     const val fastexcel = "0.18.4"
     const val jsoup = "1.19.1"
-
     const val commons_io = "2.18.0"
     const val commons_text = "1.13.0"
     const val commons_imaging = "1.0.0-alpha5"
     const val reactor_kotlin = "1.2.3"
     const val netty_resolver = "4.2.0.Final"
     const val shedlock = "6.3.1"
+
+    const val spring_security = "6.2.3" // 실제 버전은 확인 필요
+    const val langchain4j = "1.0.0-beta1"
+    const val langchain4j_b = "1.0.0-alpha1"
+    const val querydsl_version = "5.1.0"
 }
 
